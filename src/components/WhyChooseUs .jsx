ebc5233ffec52c1797c329ffbccec60d192816dd
Rownak Abdullah <rownakabdullahomi@gmail.com>
2025-02-07 11:47:15 +0600

    desugn updated

--- src/components/WhyChooseUs .jsx
+++ src/components/WhyChooseUs .jsx
@@ -54,7 +54,7 @@ const WhyChooseUs = () => {
               <img src={feature.image} alt="" />
             </div>
             <h3 className="text-xl font-semibold mb-2">{feature.title}</h3>
-            <p className="text-gray-600">{feature.description}</p>
+            <p className="">{feature.description}</p>
           </div>
         ))}
       </div>
